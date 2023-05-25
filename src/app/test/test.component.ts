68b7cefa7d3b73ee71357b92590493276494a88e
irazv <zwoniii@ya.ru>
2023-05-25 14:33:51 +0300

    first commit

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TestComponent {
+
+}
