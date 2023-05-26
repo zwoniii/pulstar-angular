896062b0e9d844a873e25ad971e489d1a8134e00
irazv <zwoniii@ya.ru>
2023-05-26 09:29:31 +0300

    advantages

---
+++ src/app/advantages/advantages.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-advantages',
+  templateUrl: './advantages.component.html',
+  styleUrls: ['./advantages.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AdvantagesComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShopComponent } from './shop/shop.component';
 import { DescriptionComponent } from './description/description.component';
+import { AdvantagesComponent } from './advantages/advantages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ShopComponent,
-    DescriptionComponent
+    DescriptionComponent,
+    AdvantagesComponent
   ],
   imports: [
     BrowserModule
