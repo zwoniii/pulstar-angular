9529da987351df80566223fba9c45092d13db6d0
irazv <zwoniii@ya.ru>
2023-05-26 09:24:39 +0300

    description

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
 import { HeaderComponent } from './header/header.component';
 import { ShopComponent } from './shop/shop.component';
+import { DescriptionComponent } from './description/description.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent,
     HeaderComponent,
-    ShopComponent
+    ShopComponent,
+    DescriptionComponent
   ],
   imports: [
     BrowserModule
